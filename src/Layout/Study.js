cb90f68b24d21836d3aca51a88fff2fa322bdf61
Tyler Forbes <forbes2121@gmail.com>
2022-05-23 08:28:13 -0400

    Switched up and simplified all the code

---
+++ src/Layout/Study.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import CardDisplay from "./CardDisplay";
+
+function Study({ deck, deckList, cardList }) {
+    const params = useParams();
+
+    return (
+        <>
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                    <li className="breadcrumb-item"><Link to={`/decks/${params.deckId}`}>{deck.name}</Link></li>
+                    <li className="breadcrumb-item active" aria-current="page">Study</li>
+                </ol>
+            </nav>
+            <h1>{deck.name}: Study</h1>
+            <CardDisplay deck={deck} cardList={cardList}/>
+        </>
+    );
+}
+
+export default Study;
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,21 +1,47 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Route, Switch, Link } from "react-router-dom";
 import Header from "./Header";
-import DeckHome from "../Decks/DeckHome";
+import NotFound from "./NotFound";
+import DeckList from "./DeckList";
+import DeckInfo from "./DeckInfo";
+import CreateDeck from "./CreateDeck";
+import { listDecks } from "./../utils/api";
 
 function Layout() {
-  return (
-    <div>
-      <Header />
-      <div className="container">
-        <Switch>
-          <Route path="/">
-            <DeckHome />
-          </Route>
-          </Switch>
-      </div>
-    </div>
-  );
+    const [deckList, setDeckList] = useState([]);
+    const addDeck = (deck) => setDeckList([...deckList, deck]);
+    const removeDeck = (deckId) => setDeckList(deckList.filter((deck) => deck.id !== deckId))
+
+    useEffect(() => {
+        async function loadDecks() {
+            const decks = await listDecks();
+            setDeckList(decks);
+        }
+        loadDecks();
+    }, []);
+
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <Switch>
+                    <Route exact path="/">
+                        <Link to="/decks/new" className="btn btn-secondary">Create Deck</Link>
+                        <DeckList deckList={deckList} removeDeck={removeDeck}/>
+                    </Route>
+                    <Route path="/decks/new">
+                        <CreateDeck addDeck={addDeck}/>
+                    </Route>
+                    <Route path="/decks/:deckId">
+                        <DeckInfo removeDeck={removeDeck} deckList={deckList}/>
+                    </Route>
+                    <Route>
+                        <NotFound />
+                    </Route>
+                </Switch>
+            </div>
+        </>
+    );
 }
 
 export default Layout;
